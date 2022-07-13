6b72604088bec9615758513cbc57e9d39051e002
gorbatkoff <gorbatkovartyom@gmail.com>
2022-07-13 14:13:51 +0300

    Delete useless function

--- src/components/BreadCrumbs/BreadCrumbs.jsx
+++ src/components/BreadCrumbs/BreadCrumbs.jsx
@@ -4,22 +4,16 @@ import Breadcrumbs from '@mui/material/Breadcrumbs';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import Link from '@mui/material/Link';
 
-function handleClick(event) {
-  event.preventDefault();
-  console.info('You clicked a breadcrumb.');
-}
-
 export default function BreadCrumbs() {
   return (
-    <div role="presentation" onClick={handleClick}>
-      <Breadcrumbs aria-label="breadcrumb" sx={{background: "none"}}
+    <div role="presentation">
+      <Breadcrumbs aria-label="breadcrumb" sx={{ background: "none" }}
         separator={<NavigateNextIcon fontSize="small" />}>
         <Link underline="hover" color="inherit" href="/">
           Arent
         </Link>
         <Link
-          underline="hover"
-          color="inherit"
+          underline="hover" color="inherit"
           href="/material-ui/getting-started/installation/"
         >
           Аренда легковых автомобилей
