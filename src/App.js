64ac50e99dac90535a911056c8ff3c0d75f58c0b
gorbatkoff <gorbatkovartyom@gmail.com>
2022-07-13 14:12:33 +0300

    Add advert page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import AdvertPage from './components/AdvertPage/AdvertPage';
 import Description from './components/Description/Description';
 import Footer from './components/Footer/Footer';
 import FooterLinks from './components/Footer/FooterLinks/FooterLinks';
@@ -12,22 +13,31 @@ import SearchMenu from './components/Search/SearchMenu/SearchMenu';
 function App() {
   return (
     <div>
-      {/* Main 
-              <Header></Header>
+      {/* Main  */}
+      {/* <Header></Header>
       <Navbar></Navbar>
       <Description></Description>
       <Promo></Promo>
       <IndexContent></IndexContent>
-      <Footer/>      
-      
-      */}
+      <Footer/>       */}
+
+
 
       {/* Search */}
 
-      <Header></Header>
+      {/* <Header></Header>
       <Navbar></Navbar>
       <Search/>
-      <Footer />
+      <Footer /> */}
+
+      {/* Advert Page */}
+
+
+      {/* <Header></Header>
+      <Navbar></Navbar>
+      <AdvertPage />
+      <Footer /> */}
+
     </div>
   );
 }
