779b522eb803e806671ff3ff769af5588b903aff
gorbatkoff <gorbatkovartyom@gmail.com>
2022-07-16 03:33:19 +0300

    Add more components

--- src/App.js
+++ src/App.js
@@ -1,43 +1,66 @@
 import './App.css';
 import AdvertPage from './components/AdvertPage/AdvertPage';
+import CreateAdvert from './components/CreateAdvert/CreateAdvert';
 import Description from './components/Description/Description';
 import Footer from './components/Footer/Footer';
 import FooterLinks from './components/Footer/FooterLinks/FooterLinks';
 import Header from './components/Header/Header';
 import IndexContent from './components/IndexContent/IndexContent';
 import Navbar from './components/Navbar/Navbar';
+import MyAdvert from './components/MyAdvert/MyAdvert';
+import ProfileEditing from './components/ProfileEditing/ProfileEditing';
 import Promo from './components/Promo/Promo';
 import Search from './components/Search/Search';
 import SearchMenu from './components/Search/SearchMenu/SearchMenu';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
     <div>
-      {/* Main  */}
+      {/* Страница: Main  */}
+
       {/* <Header></Header>
       <Navbar></Navbar>
-      <Description></Description>
+      <Description description="Легковые автомобили в Москве"></Description>
       <Promo></Promo>
       <IndexContent></IndexContent>
-      <Footer/>       */}
+      <Footer/> */}
 
-
-
-      {/* Search */}
+      {/* Страница: Search */}
 
       {/* <Header></Header>
       <Navbar></Navbar>
       <Search/>
       <Footer /> */}
 
-      {/* Advert Page */}
-
+      {/* Страница: Advert Page */}
 
       {/* <Header></Header>
       <Navbar></Navbar>
       <AdvertPage />
       <Footer /> */}
 
+      {/* Страница: CreateAdvert */}
+      {/* <Header></Header>
+      <Navbar></Navbar>
+      <CreateAdvert/> 
+      <Footer /> */}
+
+      {/* Страница: ProfileEditing */}
+      {/* <Header></Header>
+      <Navbar></Navbar>
+      <ProfileEditing /> */}
+
+      {/* Страница MyAdvert */}
+      {/* <Header></Header>
+      <Navbar></Navbar>
+      <MyAdvert/>
+      <Footer /> */}
+
+      {/* Страница Profile */}
+      {/* <Header></Header>
+      <Navbar></Navbar>
+      <Profile /> */}
     </div>
   );
 }
