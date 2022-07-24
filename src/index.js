c201e2a1c8733c9b3bb4e67597d38285d88e0a1a
gorbatkoff <gorbatkovartyom@gmail.com>
2022-07-24 20:00:09 +0300

    Add BrowserRouter wrapper

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
+
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Navbar />
+      <App />
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
