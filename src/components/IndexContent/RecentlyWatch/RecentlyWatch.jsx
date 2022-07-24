de37697873c36c5df908da4ef18339a4eb518fc0
gorbatkoff <gorbatkovartyom@gmail.com>
2022-07-24 20:04:12 +0300

    Create nested routing

--- src/components/IndexContent/RecentlyWatch/RecentlyWatch.jsx
+++ src/components/IndexContent/RecentlyWatch/RecentlyWatch.jsx
@@ -2,34 +2,23 @@ import React from 'react';
 import styles from './RecentlyWatch.module.css';
 import PromoCard from '../../Promo/PromoCard/PromoCard';
 
+import {Link} from 'react-router-dom'
+
 
 export default function RecentlyWatch() {
   return (
     <div>
-        <h2>Просмотренные объявления</h2>
-        
-        <div className={styles.announcements}>
-            <div><PromoCard/>
-                <p className={styles.description}>Москва, Пражская</p>
-                <p className={styles.description}>8 часов назад</p>            
-            </div>
-            <div><PromoCard/>
-                <p className={styles.description}>Москва, Пражская</p>
-                <p className={styles.description}>8 часов назад</p>            
-            </div>
-            <div><PromoCard/>
-                <p className={styles.description}>Москва, Пражская</p>
-                <p className={styles.description}>8 часов назад</p>            
-            </div>
-            <div><PromoCard/>
-                <p className={styles.description}>Москва, Пражская</p>
-                <p className={styles.description}>8 часов назад</p>            
-            </div>
-            <div><PromoCard/>
-                <p className={styles.description}>Москва, Пражская</p>
-                <p className={styles.description}>8 часов назад</p>            
-            </div>
+      <h2>Просмотренные объявления</h2>
+
+      <div className={styles.announcements}>
+        <div>
+          <Link to="/advert/1">
+            <PromoCard />
+            <p className={styles.description}>Москва, Пражская</p>
+            <p className={styles.description}>8 часов назад</p>
+          </Link>
         </div>
+      </div>
     </div>
   )
 }
