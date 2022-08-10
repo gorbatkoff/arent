931ca38f841972510e6abf53e70adb970d2da226
gorbatkoff <gorbatkovartyom@gmail.com>
2022-08-10 03:45:05 +0300

    Delete sx attribute

--- src/UI/Button/BasicButton.jsx
+++ src/UI/Button/BasicButton.jsx
@@ -1,19 +1,9 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import React from 'react'
 
-export default function BasicButtons({ text }) {
-    return (
-        <Button variant="contained"
-        sx={{
-            background: "#316BFE",
-            borderRadius: "7px",
-            color: "white",
-            textTransform: "none",
-            padding: "15px 27px",
-            fontSize: "14px"
-        }}
-        >
-            {text}
-        </Button>
-    );
+function BasicButton() {
+  return (
+    <div>BasicButton</div>
+  )
 }
+
+export default BasicButton
