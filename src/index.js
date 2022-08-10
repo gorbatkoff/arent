25b93ee7b2af9835226325b7662f95afa4d4df40
gorbatkoff <gorbatkovartyom@gmail.com>
2022-08-10 03:41:57 +0300

    Delete Header, Navbar etc from index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -12,10 +9,7 @@ root.render(
 
   <React.StrictMode>
     <BrowserRouter>
-      <Header />
-      <Navbar />
       <App />
-      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
