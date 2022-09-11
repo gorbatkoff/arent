b7764e8d5e43e68062cb5005b65686697646297f
gorbatkoff <gorbatkovartyom@gmail.com>
2022-09-12 01:14:54 +0300

    Add title for main page

--- src/components/IndexContent/IndexContent.jsx
+++ src/components/IndexContent/IndexContent.jsx
@@ -6,11 +6,13 @@ import RecentlyWatch from './RecentlyWatch/RecentlyWatch';
 import CommercialTransport from './CommercialTransport/CommercialTransport';
 import Moto from './Moto/Moto';
 import Catalog from './Catalog/Catalog';
+import Title from '../Title/Title';
 
 function IndexContent() {
   return (
-    <Container sx={{padding: "100px 0"}}>
-      
+    <Container sx={{padding: "10px 0"}}>
+        <Title title="Легковые автомобили в Москве"></Title>
+        <p style={{ fontWeight: '400', color: '#78839E', fontSize: '16px', paddingBottom: "5em" }}>7 532 предложения</p>
         <Advert/>
         <RecentlyWatch/>
         <CommercialTransport/>
